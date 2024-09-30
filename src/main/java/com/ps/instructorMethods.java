a8443ef710fa21f38f59c309f954a9aeccff0fd4
Debani <debani.cossio1217@gmail.com>
2024-09-30 11:09:41 -0500

    added the addition method

--- src/main/java/com/ps/instructorMethods.java
+++ src/main/java/com/ps/instructorMethods.java
@@ -1,6 +1,14 @@
 package com.ps;
 
-public class instructorMethods {
+class Calculator {
+    public static void main(String[] args) {
+        int sum = addition( numberOne:473, numberTwo:298);
+        System.out.println(sum);
+    }
+
+    public static int addition(int numberOne, int numberTwo) {
+        return numberOne + numberTwo;
 
 
+    }
 }
